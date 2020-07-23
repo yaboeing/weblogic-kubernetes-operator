ce8b06260caaca9736976ba23e1ea68b3be4bee4
doxiao <dongbo.xiao@oracle.com>
2020-07-23 12:30:54 +0000

    Improve test

--- new-integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMiiDomain.java
+++ new-integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMiiDomain.java
@@ -12,6 +12,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 import io.kubernetes.client.custom.V1Patch;
@@ -47,6 +48,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static oracle.weblogic.kubernetes.TestConstants.ADMIN_SERVER_NAME_BASE;
@@ -1045,48 +1047,65 @@ class ItMiiDomain {
       String internalPort,
       String appPath
   ) {
-    boolean v2AppAvailable = false;
+    with().pollDelay(100, MILLISECONDS)
+        .and().with().pollInterval(400, MILLISECONDS)
+        .atMost(10, MINUTES).await().conditionEvaluationListener(
+        condition -> logger.info("Waiting for application to be patched in namespace {0} "
+                + "(elapsed time {1}ms, remaining time {2}ms)",
+            namespace,
+            condition.getElapsedTimeInMS(),
+            condition.getRemainingTimeInMS()))
+        .until(assertDoesNotThrow(
+            () -> appAvailabilityCheckCompleted(
+                namespace, appAvailability, managedServerPrefix, replicaCount, internalPort, appPath),
+            String.format(
+                "Checking availability of application %s in namespace %s failed.",
+                appPath, namespace)));
+  }
 
-    // Access the pod periodically to check application's availability across the duration
-    // of patching the domain with newer version of the application.
-    while (!v2AppAvailable)  {
-      v2AppAvailable = true;
+  private static Callable<Boolean> appAvailabilityCheckCompleted(
+      String namespace,
+      List<Integer> appAvailability,
+      String managedServerPrefix,
+      int replicaCount,
+      String internalPort,
+      String appPath
+  ) {
+    return () -> {
+      // Access the pod periodically to check application's availability across the duration
+      // of patching the domain with newer version of the application.
+      boolean v2AppAvailable = true;
       for (int i = 1; i <= replicaCount; i++) {
         v2AppAvailable = v2AppAvailable && appAccessibleInPodKubectl(
-                            namespace,
-                            managedServerPrefix + i, 
-                            internalPort, 
-                            appPath, 
-                            MII_APP_RESPONSE_V2 + i);
+            namespace,
+            managedServerPrefix + i,
+            internalPort,
+            appPath,
+            MII_APP_RESPONSE_V2 + i);
       }
 
       int count = 0;
       for (int i = 1; i <= replicaCount; i++) {
         if (appAccessibleInPodKubectl(
             namespace,
-            managedServerPrefix + i, 
-            internalPort, 
-            appPath, 
-            "Hello World")) {  
+            managedServerPrefix + i,
+            internalPort,
+            appPath,
+            "Hello World")) {
           count++;
         }
       }
       appAvailability.add(count);
-      
+
       if (count == 0) {
-        logger.info("XXXXXXXXXXX: application not available XXXXXXXX");
-        break;
+        logger.info("Neither V1 nor V2 application available!");
       } else {
-        logger.fine("YYYYYYYYYYY: application available YYYYYYYY count = " + count);   
+        logger.fine("Either V1 or V2 application, or both available count = " + count);
       }
-      try {
-        TimeUnit.MILLISECONDS.sleep(200);
-      } catch (InterruptedException ie) {
-        // do nothing
-      }
-    }
+      return v2AppAvailable || (count == 0);
+    };
   }
-  
+
   private static boolean appAlwaysAvailable(List<Integer> appAvailability) {
     for (Integer count: appAvailability) {
       if (count == 0) {
